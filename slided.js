0fb6539ad4219b9a1d0e7369d5b00e53b89aa699
Fardin <93711200+yfp27@users.noreply.github.com>
2023-12-29 21:59:52 +0600

    Add files via upload

---
+++ slided.js
@@ -0,0 +1,18 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("avtive");
+      } else {
+        reveals[i].classList.remove("avtive");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
